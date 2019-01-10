6c87d47e3433449966ed9335a3580777643b5a5c
Ryan Welcher <ryan.welcher@10up.com>
2019-01-10 13:31:18 -0500

    Setup completed

--- .storybook/config.js
+++ .storybook/config.js
@@ -8,14 +8,10 @@ import * as data from '@wordpress/data';
 import * as editor from "@wordpress/editor";
 import * as components from "@wordpress/components";
 import * as i18n from "@wordpress/i18n";
-import React from 'react';
-
-import "./style.css";
-window.React = React;
-
 import { registerCoreBlocks } from '@wordpress/block-library';
-registerCoreBlocks();
 
+// Config styles
+import "./style.css";
 
 
 // Setup.
@@ -27,27 +23,30 @@ wp.editor = editor;
 wp.components = components;
 wp.i18n = i18n;
 
+//
+registerCoreBlocks();
 
 addDecorator( centered );
 addDecorator(
     withOptions(
         {
-        /**
-         * name to display in the top left corner
-         * @type {String}
-         */
-        name: 'Gutenberg Blocks',
-        /**
-         * show addon panel as a vertical panel on the right
-         * @type {Boolean}
-         */
-        addonPanelInRight: true,
-      }
+          /**
+           * name to display in the top left corner
+           * @type {String}
+           */
+          name: 'Gutenberg Blocks',
+          /**
+           * show addon panel as a vertical panel on the right
+           * @type {Boolean}
+           */
+          addonPanelInRight: true,
+        }
     )
 )
 
+
 function loadStories() {
-  require('../src/stories');
+  require('../stories/index.js' );
 }
 
 configure(loadStories, module);
