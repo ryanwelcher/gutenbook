8dba8e43b430b676f75b8c8a6d24d9d6e8b069ef
Ryan Welcher <ryan.welcher@10up.com>
2020-01-17 15:14:06 -0500

    Getting Storybook loading and displaying the block editor

---
+++ .storybook/theme.js
@@ -0,0 +1,21 @@
+/**
+ * External dependencies
+ */
+import { create } from '@storybook/theming/create';
+
+export default create( {
+	base: 'light',
+	colorPrimary: '#fefefe',
+  	colorSecondary: '#585858',
+	textColor: '#fefefe',
+	textInverseColor: 'black',
+	appBg: '#1a1a1a',
+	appContentBg: '#fefefe',
+	barBg: '#1a1a1a',
+	barTextColor: '#585858',
+	barSelectedColor: '#fefefe',
+	appBorderRadius: 0,
+	brandTitle: '10up',
+	brandUrl: 'https://10up.com',
+	brandImage:'https://10up.com/wp-content/themes/10up-sept2016/assets/img/10up-logo-full.svg',
+} );
